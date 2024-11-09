e548f2cb24d53aad2266e92fd95e719cdac1b720
Richard Abishai <rickoshade1891@gmail.com>
2024-11-09 21:01:12 +0530

    Import Modified

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import { useUsers } from '@/context/UsersContext';
 
 export default function Navbar() {
